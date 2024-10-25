0acf4226463f7cbcea0d34ac201937113cddfd08
joimb9064 <joimb9064@oslomet.no>
2024-10-25 13:03:11 +0200

    update

--- VRPTW-ACO-python/basic_aco.py
+++ VRPTW-ACO-python/basic_aco.py
@@ -15,7 +15,6 @@ class BasicACO:
     def __init__(self, graph: VrptwGraph, ants_num=10, max_iter=100, alpha=1.0, beta=2, q0=0.1,
                  whether_or_not_to_show_figure=True, cost_of_violation=100):
         super()
-        # Add the alpha parameter to the class
         self.alpha = alpha
         self.graph = graph
         self.ants_num = ants_num
@@ -23,15 +22,14 @@ class BasicACO:
         self.max_load = graph.vehicle_capacity
         self.beta = beta
         self.q0 = q0
-        # best path
         self.best_path_distance = None
         self.best_path = None
         self.best_vehicle_num = None
-        self.ants = None  # Add this line to initialize the ants attribute
-        self.basicACO_results = []  # Add this line to initialize the basicACO_results attribute 
-        # 30 September, added to minimize the phermone to routes with Penalties
-        self.cost_of_violation = cost_of_violation  # Add this line 
+        self.ants = None  # Initialize ants
+        self.basicACO_results = []  # Initialize results attribute 
+        self.cost_of_violation = cost_of_violation  # Cost of violation
         self.whether_or_not_to_show_figure = whether_or_not_to_show_figure
+        self.terminate_program = False  # Termination flag
 
     def run_basic_aco(self):
         path_queue_for_figure = Queue()
@@ -41,7 +39,6 @@ class BasicACO:
             figure = VrptwAcoFigure(self.graph.nodes, path_queue_for_figure)
             figure.run()
         basic_aco_thread.join()
-        # Call the method here
 
         if self.whether_or_not_to_show_figure:
             path_queue_for_figure.put(PathMessage(None, None))
@@ -49,12 +46,15 @@ class BasicACO:
     def _basic_aco(self, path_queue_for_figure: Queue):
         start_time_total = time.time()
         start_iteration = 0
-        # Clear the results list at the start of each run
         global results
         results = []
-        # 30 September, added to minimize the phermone to routes with Penalties
+
         for iter in range(self.max_iter):
-            self.ants = list(Ant(self.graph, cost_of_violation=self.cost_of_violation) for _ in range(self.ants_num))
+            if self.terminate_program:  # Check termination flag
+                print("Program termination requested. Exiting after current iteration.")
+                break
+
+            self.ants = [Ant(self.graph, cost_of_violation=self.cost_of_violation) for _ in range(self.ants_num)]
             for k in range(self.ants_num):
                 next_index = 0
                 self.ants[k].move_to_next_index(next_index, iter, self.best_vehicle_num)
@@ -73,10 +73,6 @@ class BasicACO:
 
                 self.ants[k].move_to_next_index(0, iter, self.best_vehicle_num)
                 self.graph.local_update_pheromone(self.ants[k].current_index, 0, 0)  # No penalty for depot
-         # 30 September, added to minimize the phermone to routes with Penalties
-                # Only append the result if the next_index is a depot
-                if self.graph.nodes[next_index].is_depot:
-                    results.append(results)
 
             paths_distance = np.array([ant.total_travel_distance for ant in self.ants])
 
@@ -99,28 +95,26 @@ class BasicACO:
                 if self.whether_or_not_to_show_figure:
                     path_queue_for_figure.put(PathMessage(self.best_path, self.best_path_distance))
 
-                print('\n')
-                print('[iteration %d]: find a improved path, its distance is %f' % (iter, self.best_path_distance))
-                print('The iteration path is: ', self.best_path)  # Print the best path
-                print('it takes %0.3f second multiple_ant_colony_system running' % (time.time() - start_time_total))
+                print('\n[iteration %d]: found an improved path, distance is %f' % (iter, self.best_path_distance))
+                print('The iteration path is: ', self.best_path)
+                print('it takes %0.3f seconds ant_colony_system running' % (time.time() - start_time_total))
 
             self.graph.global_update_pheromone(self.best_path, self.best_path_distance)
 
             given_iteration = 100
             if iter - start_iteration > given_iteration:
-                print('\n')
-                print('iteration exit: can not find better solution in %d iteration' % given_iteration)
+                print('Exiting: No better solution found in %d iterations' % given_iteration)
                 break
+
         self.write_basicACO_to_csv(self.basicACO_results)
         self.ants[0].write_to_csv()
-        
-        print('\n')
-        print('final best path distance is %f, number of vehicle is %d' % (self.best_path_distance, self.best_vehicle_num))
-        print('The best path is: ', self.best_path)  # Print the best path
-        print('it takes %0.3f second multiple_ant_colony_system running' % (time.time() - start_time_total))
+
+        print('\nFinal best path distance is %f, number of vehicles is %d' % (self.best_path_distance, self.best_vehicle_num))
+        print('The best path is: ', self.best_path)
+        print('it takes %0.3f seconds ant_colony_system running' % (time.time() - start_time_total))
 
     def write_basicACO_to_csv(self, basicACO_results):
-        csv_dir = "/Users/josephimbien/desktop/EAI_project_problems/VRPTW-ACO-python/csv"  # specify your directory here
+        csv_dir = "/Users/josephimbien/desktop/EAI_project_problems/VRPTW-ACO-python/csv"
         now = datetime.datetime.now()
         now_str = now.strftime("%Y-%m-%d-%H-%M-%S")
         csv_path = os.path.join(csv_dir, f'basicACO_results_{now_str}.csv')
@@ -128,51 +122,52 @@ class BasicACO:
         with open(csv_path, 'w', newline='') as csvfile:
             fieldnames = ['Iteration', 'Improved path distance', 'Iteration path', 'Execution time', 'Total travel distance', 'Number of vehicles used']
             writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
-
             writer.writeheader()
-            for basicACO_result in basicACO_results:
-                writer.writerow(basicACO_result)
-    def select_next_index(self, ant): # Update this part September.2024.
-        """
-        Select the next node
-        :param ant:
-        :return:
-        """
+            for result in basicACO_results:
+                writer.writerow(result)
+
+    def select_next_index(self, ant):
         current_index = ant.current_index
         index_to_visit = ant.index_to_visit
-        # 30 September, added to minimize the phermone to routes with Penalties
-        # Calculate penalties for each potential next node
+
+        if len(index_to_visit) == 0:
+            #print("No more nodes to visit. Flagging for termination.") # Turn this on for debugging but by default it should be off.
+            self.terminate_program = True  # Set the termination flag
+            return 0  # Return to depot or terminate this ant's tour
+
         penalties = np.array([ant.calculate_penalty(i) for i in index_to_visit])
-        # 30 September, added to minimize the pheromone to routes with Penalties
-        # Adjust heuristic information based on penalties
         heuristic_with_penalty = np.array([self.graph.calculate_heuristic_with_penalty(current_index, i) for i in index_to_visit])
 
-
-        # Use self.alpha to adjust the importance of pheromone information
         transition_prob = np.power(self.graph.pheromone_mat[current_index][index_to_visit], self.alpha) * \
-            np.power(self.graph.heuristic_info_mat[current_index][index_to_visit], self.beta)
+                          np.power(self.graph.heuristic_info_mat[current_index][index_to_visit], self.beta)
+
+        if np.sum(transition_prob) == 0:
+            print("No valid transition probabilities. Flagging for termination.")
+            self.terminate_program = True  # Set the termination flag
+            return 0  # Return to depot or terminate this ant's tour
+
         transition_prob = transition_prob / np.sum(transition_prob)
 
         if np.random.rand() < self.q0:
             max_prob_index = np.argmax(transition_prob)
             next_index = index_to_visit[max_prob_index]
         else:
-            # Use roulette wheel selection algorithm
             next_index = BasicACO.stochastic_accept(index_to_visit, transition_prob)
+
         return next_index
+
     @staticmethod
     def stochastic_accept(index_to_visit, transition_prob):
-        # calculate N and max fitness value
+        if len(index_to_visit) == 0:
+            return 0  # Return to depot or terminate
+
         N = len(index_to_visit)
-
-        # normalize
         sum_tran_prob = np.sum(transition_prob)
-        norm_transition_prob = transition_prob/sum_tran_prob
+        if sum_tran_prob == 0:
+            return 0  # Handle the termination or depot return
 
-        # select: O(1)
+        norm_transition_prob = transition_prob / sum_tran_prob
         while True:
-            # randomly select an individual with uniform probability
             ind = int(N * random.random())
             if random.random() <= norm_transition_prob[ind]:
                 return index_to_visit[ind]
-            
--- VRPTW-ACO-python/vprtw_aco_figure.py
+++ VRPTW-ACO-python/vprtw_aco_figure.py
@@ -1,6 +1,7 @@
 import matplotlib.pyplot as plt
 from multiprocessing import Queue as MPQueue
-
+import os
+import datetime
 
 class VrptwAcoFigure:
     def __init__(self, nodes: list, path_queue: MPQueue):
@@ -11,39 +12,87 @@ class VrptwAcoFigure:
         self._depot_color = 'k'
         self._customer_color = 'steelblue'
         self._line_color = 'darksalmon'
+        self.best_distance = float('inf')  # Initialize the best distance to a very high value
+        self.best_path_info = None  # To store the best path's info
 
     def _draw_point(self):
         self.figure_ax.scatter([self.nodes[0].x], [self.nodes[0].y], c=self._depot_color, label='depot', s=40)
         self.figure_ax.scatter(list(node.x for node in self.nodes[1:]),
                                list(node.y for node in self.nodes[1:]), c=self._customer_color, label='customer', s=20)
-        plt.pause(0.5)
+        plt.pause(0.01)
 
     def run(self):
         self._draw_point()
-        self.figure.show()
+        plt.pause(0.01)
 
         while True:
-            if not self.path_queue.empty():
-                info = self.path_queue.get()
-                while not self.path_queue.empty():
-                    info = self.path_queue.get()
-            # Retrieve penalty from info , 30 September 2024.
-                path, distance, used_vehicle_num , penalty = info.get_path_info()
-                if path is None:
+            try:
+                # Wait for a short time for an item to appear in the queue
+                info = self.path_queue.get(timeout=10)  # Wait for 10 seconds for new data
+                if info is None:
                     print('[draw figure]: exit')
                     break
 
+                # Retrieve info from queue
+                path, distance, used_vehicle_num, penalty = info.get_path_info()
+
+                # Update only if this is the best path so far
+                if distance < self.best_distance:
+                    self.best_distance = distance
+                    self.best_path_info = (path, distance, used_vehicle_num, penalty)  # Update best path info
+
+                # Clear the old lines from the figure
                 lines_to_remove = [line for line in self.figure_ax.lines if line.get_label() == 'line']
                 for line in lines_to_remove:
-                    line.remove()  # Remove line from axes
-            # Include penalty in the title , 30 September 2024.
-                self.figure_ax.set_title('travel distance: %0.2f, number of vehicles: %d, penalty: %0.2f' % (distance, used_vehicle_num, penalty))
+                    line.remove()
+
+                # Update the plot title with the path info
+                self.figure_ax.set_title(
+                    'Travel Distance: %.2f, Vehicles: %d, Penalty: %.2f' % 
+                    (distance, used_vehicle_num, penalty)
+                )
                 self._draw_line(path)
-            plt.pause(1)
+
+                plt.pause(0.01)
+
+            except Exception as e:
+                print("Timeout or queue is empty, exiting loop.")
+                break  # Exit the loop if no data is received
+
+        # After the loop ends, save the final best path
+        if self.best_path_info:
+            self._save_final_best_path()
+
+    def _save_final_best_path(self):
+        path, distance, used_vehicle_num, penalty = self.best_path_info
+
+        # Save the figure with date and time in the filename
+        output_dir = "/Users/josephimbien/Desktop/unitesting Problem 3 ACO/VRPTW-ACO-python/image"
+        
+        if not os.path.exists(output_dir):
+            os.makedirs(output_dir)
+
+        # Get current time for the filename
+        current_time = datetime.datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
+
+        # Set the plot title to include the final best path
+        self.figure_ax.set_title(
+            'Final Best Path - Travel Distance: %.2f, Vehicles: %d, Penalty: %.2f' % 
+            (distance, used_vehicle_num, penalty)
+        )
+
+        # Save the plot with the date and time in the filename
+        output_file_path = os.path.join(output_dir, f"final_best_path_distance_{distance:.2f}_vehicles_{used_vehicle_num}_{current_time}.png")
+        self.figure.savefig(output_file_path, format='png')  # Save the plot as .png
+        print(f"Successfully saved final best path as: {output_file_path}")
+        print("Exiting gracefully after saving the final best path.")
+
+        # Exit the figure loop cleanly to prevent further updates
+        plt.close(self.figure)
 
     def _draw_line(self, path):
         for i in range(1, len(path)):
             x_list = [self.nodes[path[i - 1]].x, self.nodes[path[i]].x]
             y_list = [self.nodes[path[i - 1]].y, self.nodes[path[i]].y]
             self.figure_ax.plot(x_list, y_list, color=self._line_color, linewidth=1.5, label='line')
-            plt.pause(0.2)
+            plt.pause(0.01)
